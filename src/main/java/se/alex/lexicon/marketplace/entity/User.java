ab1c07a9ee8898a715ff1b0e08d6b88048a22d42
hostghost <ALex.youssef@live.com>
2024-10-17 10:47:30 +0200

    Created User entity with rols email,username, and passwordsfields

---
+++ src/main/java/se/alex/lexicon/marketplace/entity/User.java
@@ -0,0 +1,35 @@
+package se.alex.lexicon.marketplace.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    public enum Role {
+        BUYER,
+        SELLER
+    }
+}
