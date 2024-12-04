fa7771b93000dd48e0cbac9d98387611253fb25d
Alex Youssef <alex.youssef@live.com>
2024-12-04 10:52:57 +0200

    Set up ESLint and Prettier for code linting and formatting

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+// frontend/.eslintrc.js
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+    },
+};
